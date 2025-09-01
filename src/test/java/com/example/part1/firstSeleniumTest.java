f71b3ccb9e8d4d70f5993594144b139b5df42141
HANANBEN <hbenazize@gmail.com>
2025-09-01 19:34:59 +0200

    test login feature

--- src/test/java/com/example/part1/firstSeleniumTest.java
+++ src/test/java/com/example/part1/firstSeleniumTest.java
@@ -2,17 +2,23 @@ package com.example.part1;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
+
 
 public class firstSeleniumTest {
 
 
-    WebDriver driver;
+    static WebDriver driver;
 
     // the goal of a web driver is to controle the browser and help fon the elements 
      @BeforeClass
-     public void setUp(){
+     public static void setUp(){
         driver = new ChromeDriver(); 
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); 
@@ -22,13 +28,52 @@ public class firstSeleniumTest {
 
 
      @AfterClass
-      public void tearDown(){
-           driver.quit();
+      public static void tearDown(){
+          //  driver.quit();
       }
 
       @Test 
-      public void testLoggingIntoApplication(){
-        driver.findElement(By.)
+      public void testLoggingIntoApplication() throws InterruptedException{
+
+
+        Thread.sleep(2000);
+
+        /**
+         * 1. find an element 
+         * 2. perform actionon an element 
+         * 
+         * BY is A CLASS that is used to locate elements by selenium locators :
+         * 4 locators in selenium  :
+         * By.className : 
+         * By.cssSelector :
+         * By.id :
+         * By.linkText :
+         * By.name
+         * By.partialLinkTest
+         * By.tagName 
+         * By.xpath
+         */
+        WebElement username = driver.findElement(By.name("username")); 
+
+        username.sendKeys("admin");
+
+
+
+        var password = driver.findElement(By.name("password")); 
+
+        password.sendKeys("admin123");
+         
+        // we checked our dom , and we find out that one button exist , so we can select the button using a tag name which is "button"
+        driver.findElement(By.tagName("button") ).click();
+        
+
+        Thread.sleep(4000);
+        
+        String actualResult =driver.findElement(By.tagName("h6")).getText();
+
+        Assert.assertEquals("Dashboard" , actualResult);
+
+
       }
 
 
